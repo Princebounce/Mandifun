9374b93ecbdbe2d5e52c1c7920649fe01172b04f
Lawal Taofik <layomide81@gmail.com>
2024-03-21 11:58:45 +0100

    fixed shadow drop. mt and mb

--- mandifun/src/components/Banner/Banner.jsx
+++ mandifun/src/components/Banner/Banner.jsx
@@ -14,7 +14,7 @@ const Banner = () => {
             <img
               src={BannerImg}
               alt=""
-              className="max-w-[400px] h-[350px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover"
+              className="max-w-[400px] h-[350px] w-full mx-auto drop-shadow-[-5px_10px_12px_rgba(0,0,0,0.4)] object-cover"
             />
           </div>
           {/* Text details section  */}
