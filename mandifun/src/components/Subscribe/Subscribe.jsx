8fdfb7356c82756385606184f7ea13dd2d76b590
Lawal Taofik <layomide81@gmail.com>
2024-03-21 11:59:14 +0100

    added send button

--- mandifun/src/components/Subscribe/Subscribe.jsx
+++ mandifun/src/components/Subscribe/Subscribe.jsx
@@ -18,9 +18,17 @@ const Subscribe = () => {
       style={BannerImg}
     >
       <div className="container backdrop-blur-sm py-10">
-        <div className="space-y-5 max-w-xl mx-auto">
-          <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold">Get Notifieed About New Products</h1>
-          <input data-aos="fade-up" placeholder="Enter your Email" type="text" className="w-full p-3 " />
+        <div className="max-w-xl mx-auto">
+          <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold">Get Notified About New Products</h1>
+          <div className="flex justify-center sm:justify-start items-center mt-5">
+            <input
+              data-aos="fade-up"
+              placeholder="Enter your Email"
+              type="text"
+              className="w-full p-3 mr-2"
+            />
+            <button className="p-3 bg-blue-500 text-white rounded-md">Send</button>
+          </div>
         </div>
       </div>
     </div>
